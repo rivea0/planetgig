38591290ec55f86865aab4a16114b1b3770e2950
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-07-30 15:58:01 +0300

    Fix route name

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 //const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/gigs/:id', '/api/webhook/clerk']);
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/gigs/:id', '/api/webhooks/clerk']);
 
 
 export default clerkMiddleware((auth, request) => {
