6b28b5cbc1dd84f7159d8e9b65d4f8a8e476aa3f
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-08-03 02:35:07 +0300

    Update artist profile and edit profile pages

--- src/app/(root)/profile/[username]/page.tsx
+++ src/app/(root)/profile/[username]/page.tsx
@@ -1,14 +1,20 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import {
   getAllUsers,
   getArtistByUsername,
   getCurrentUser,
 } from '@/lib/actions/user.action'
-import Bio from '@/app/components/shared/Bio'
-import SpotifyWidget from '@/app/components/shared/SpotifyWidget'
-import SocialLinks from '@/app/components/shared/SocialLinks'
+import ArtistOverview from '@/app/components/shared/ArtistOverview'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/app/components/ui/tabs'
+import ArtistHeader from '@/app/components/shared/ArtistHeader'
+import ArtistReviews from '@/app/components/shared/ArtistReviews'
+import { ArtistType } from '@/types'
 import { Button } from '@/app/components/ui/button'
+import Link from 'next/link'
 
 export async function generateStaticParams() {
   const users = await getAllUsers()
@@ -26,50 +32,53 @@ export default async function Profile({
   let user
   try {
     user = await getCurrentUser()
-  } catch(err) {
+  } catch (err) {
     user = null
   }
 
-  const artist = await getArtistByUsername(params.username)
+  const artist = (await getArtistByUsername(params.username)) as ArtistType & {
+    _id: string
+  }
   const isOwnPage = user?.username === artist?.username
-  // user.username is not the params.username, show add comment
+
   if (artist) {
-    // const spotifyId = artist?.socialLinks.get('spotify')!
     return (
       artist && (
-        <div className='bg-coffee-50'>
-          <div className="flex justify-center gap-24 py-8">
-            <Image
-              src={artist.photo}
-              alt="Artist image"
-              width={100}
-              height={100}
-              className="rounded-md"
-            />
-            <h1 className="text-4xl self-center">{artist.nameToDisplay || artist.username}</h1>
-            {isOwnPage && <Link href="/profile/edit">edit</Link>}
+        <div className="bg-coffee-50 flex flex-col min-h-screen">
+          <ArtistHeader
+            artist={JSON.parse(JSON.stringify(artist))}
+            isOwnPage={isOwnPage}
+          />
+          <div className="flex justify-center items-center gap-4 py-8">
+            <Tabs defaultValue="overview" className="w-full flex flex-col">
+              <TabsList className="self-center">
+                <TabsTrigger value="overview">Overview</TabsTrigger>
+                <TabsTrigger value="reviews" className="ml-4">
+                  Reviews
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="overview" className="w-full">
+                <ArtistOverview
+                  artist={JSON.parse(JSON.stringify(artist))}
+                  isOwnPage={isOwnPage}
+                />
+              </TabsContent>
+              <TabsContent value="reviews">
+                <ArtistReviews artistId={artist._id} />
+              </TabsContent>
+            </Tabs>
           </div>
-          <SocialLinks />
-          <div className="flex px-8 mt-12 justify-center">
-            <Bio text="bio section" />
-            {/* example */}
-            <SpotifyWidget spotifyId={'06HL4z0CvFAxyc27GXpf02'} />
-          </div>
-          {artist.images?.map((image) => {
-            return (
-              <Image src={image} alt="" width={400} height={250} />
-            )
-          })}
-          {artist.videos?.map((video) => {
-            return (
-              <div>{video}</div>
-            )
-          })}
-      {!isOwnPage && <Button type="button">Add review</Button>}
+          {!isOwnPage && (
+            <Button type="button" className="self-center mb-3">
+              <Link href={`/review/${artist.username}`}>
+                Add review to artist
+              </Link>
+            </Button>
+          )}
         </div>
       )
     )
   }
 
-  return <>This artist does not exist!</>
+  return <div>This artist does not exist!</div>
 }
--- src/app/(root)/profile/edit/page.tsx
+++ src/app/(root)/profile/edit/page.tsx
@@ -1,7 +1,5 @@
-// import { auth } from "@clerk/nextjs/server"
 import EditProfileForm from '@/app/components/shared/EditProfileForm'
-import { getCurrentUser } from '@/lib/actions/user.action'
-
+import { getCurrentUser, getArtistByClerkId } from '@/lib/actions/user.action'
 export default async function EditProfile() {
   let user
   try {
@@ -9,16 +7,19 @@ export default async function EditProfile() {
   } catch (err) {
     user = null
   }
+  if (user) {
+    const artist = await getArtistByClerkId(user.id)
 
-  // const { sessionClaims } = auth()
-  // const userId = sessionClaims?.userId as string
+    return (
+      <div className="flex flex-col justify-center items-center bg-coffee-50">
+        <h1 className="text-3xl mt-8">Edit Profile</h1>
+        <EditProfileForm
+          userId={user.id}
+          artist={JSON.parse(JSON.stringify(artist))}
+        />
+      </div>
+    )
+  }
 
-  return user ? (
-    <div className="flex flex-col justify-center items-center bg-coffee-100">
-      <h1 className="text-3xl mt-8">Edit Profile</h1>
-      <EditProfileForm userId={user.id} />
-    </div>
-  ) : (
-    <div>User does not exist</div>
-  )
+  return <div>User does not exist</div>
 }
